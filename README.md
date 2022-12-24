ae6c718a38d8a67061807a71c8e60c6aba664638
Benjamin Heng <benjamin.heng@thecarousell.com>
2022-12-25 03:15:33 +0800

    Parse +board config option
    I'm just going for a a straightforward implementation for now just to
    get the proof of concept working. I'd like to refactor the structure by
    implementing a lexer instead.

---
+++ README.md
@@ -0,0 +1,5 @@
+TODO:
+
+- Switch to a lexer approach. If there's a syntax error I want to be able to
+  print the line and character rather than a generic error. It'll also be a
+  good opportunity to play with building a lexer.
--- main.go
+++ main.go
@@ -5,20 +5,106 @@ package main
 // #include "pikchr.h"
 import "C"
 import (
-	"fmt"
+	"errors"
+	"io/ioutil"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
 	"unsafe"
 )
 
 func main() {
-	source := `
-arrow right 200% "Markdown" "Source"
-box rad 10px "Markdown" "Formatter" "(markdown.c)" fit
-arrow right 200% "HTML+SVG" "Output"
-arrow <-> down from last box.s
-box same "Pikchr" "Formatter" "(pikchr.c)" fit
-`
-	svg := pikchr(source)
-	fmt.Println(svg)
+	err := execute()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func execute() error {
+	filename := "examples/board.tetris-svg"
+	f, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	source, err := ioutil.ReadAll(f)
+	if err != nil {
+		return err
+	}
+
+	// Variables for the `board` config
+	var boardModeEnabled bool
+	var boardWidth, boardHeight int64
+
+	configStatementsAllowed := true
+
+	lines := strings.Split(string(source), "\n")
+	for _, line := range lines {
+		// Configuration options start with +
+		if strings.HasPrefix(line, "+") {
+			if configStatementsAllowed {
+				configKey, configValue, err := getConfigValue(line)
+				if err != nil {
+					return err
+				}
+				switch configKey {
+				case "board":
+					x, y, err := validateBoardConfig(configValue)
+					if err != nil {
+						return err
+					}
+					boardWidth = x
+					boardHeight = y
+					boardModeEnabled = true
+				}
+			} else {
+				return errors.New("config statements must only appear at the beginning of the file")
+			}
+		} else if line != "\n" {
+			// Config statements are only allowed at the beginning
+			// of the file, excluding newlines. Once we start
+			// seeing other statements, we stop reading configs.
+			configStatementsAllowed = false
+
+			// TODO: parse our DSL here
+			_ = boardModeEnabled
+			_ = boardWidth
+			_ = boardHeight
+		}
+	}
+	return nil
+}
+
+func validateBoardConfig(value string) (x, y int64, err error) {
+	xy := strings.SplitN(value, "x", 2)
+	if len(xy) != 2 {
+		return 0, 0, errors.New("invalid board config value")
+	}
+	sx := xy[0]
+	sy := xy[1]
+	x, err = strconv.ParseInt(sx, 10, 64)
+	if err != nil {
+		return 0, 0, err
+	}
+	y, err = strconv.ParseInt(sy, 10, 64)
+	if err != nil {
+		return 0, 0, err
+	}
+	return x, y, nil
+}
+
+func getConfigValue(line string) (key string, value string, err error) {
+	if !strings.HasPrefix(line, "+") {
+		return "", "", errors.New("config statement not found")
+	}
+	re := regexp.MustCompile(`\+([a-zA-Z][a-zA-Z0-9_-]+) (.+)`)
+	matches := re.FindStringSubmatch(line)
+	if len(matches) != 3 {
+		return "", "", errors.New("cannot parse config statement")
+	}
+	return matches[1], matches[2], nil
 }
 
 func pikchr(source string) string {
